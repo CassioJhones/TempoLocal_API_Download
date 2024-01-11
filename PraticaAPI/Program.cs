0bb9ae1651165488e6acad214ca08cfc855bfaee
CASSIO <cassio.bjhones@gmail.com>
2024-01-11 03:00:26 -0300

    loop

--- PraticaAPI/Program.cs
+++ PraticaAPI/Program.cs
@@ -1,33 +1,37 @@
 ﻿using PraticaAPI.Filtro;
 using PraticaAPI.Modelos;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 Console.Title = "Tempo Local";
-
-Console.WriteLine("\nInsira uma cidade: ");
-using (HttpClient chamada = new())
+while (true)
 {
-    string endPoint = $"https://api.openweathermap.org/data/2.5/weather?q={Console.ReadLine()}&appid={Function.Chave()}";
-    try
+    Console.WriteLine("\nInsira uma cidade: ");
+    using (HttpClient chamada = new())
     {
-        string resposta = await chamada.GetStringAsync(endPoint);
-        Root? cidade = JsonSerializer.Deserialize<Root>(resposta);
-        double tempCelcius = Function.KelvinToCelcius(cidade!.Main.Temp);
-        double veloKM = Function.MsToKm(cidade.Wind.Speed);
+        string endPoint = $"https://api.openweathermap.org/data/2.5/weather?q={Console.ReadLine()}&appid={Function.Chave()}";
+        try
+        {
+            string resposta = await chamada.GetStringAsync(endPoint);
+            Root? cidade = JsonSerializer.Deserialize<Root>(resposta);
+            double tempCelcius = Function.KelvinToCelcius(cidade!.Main.Temp);
+            double veloKM = Function.MsToKm(cidade.Wind.Speed);
 
-        Console.WriteLine($"\n{cidade.Name} com pressão atmosferica de {cidade.Main.Pressure}hPa");
-        Console.WriteLine($"Os ventos estão há {veloKM} Km/H");
-        Console.WriteLine($"Temperatura de {tempCelcius} °C");
-        Console.WriteLine($"Geolocalização {cidade.Coord.Lat} {cidade.Coord.Lon}");
+            Console.WriteLine($"\n{cidade.Name} com pressão atmosferica de {cidade.Main.Pressure / 1000}bar");
+            Console.WriteLine($"Os ventos estão há {veloKM} Km/H");
+            Console.WriteLine($"Temperatura de {tempCelcius} °C");
+            Console.WriteLine($"Geolocalização {cidade.Coord.Lat} {cidade.Coord.Lon}");
 
-        Console.ReadKey();
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        catch (HttpRequestException)
+        {
+            Console.WriteLine("Nome Nao Encontrado");
+        }
+
     }
-
-    catch (HttpRequestException)
-    {
-        Console.WriteLine("Nome Nao Encontrado");
-    }
-
 }
 /*
 
