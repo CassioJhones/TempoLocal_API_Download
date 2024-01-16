eaf05d9cfd1cf5b582b6a1239906e1d4600a8b5b
CASSIO <cassio.bjhones@gmail.com>
2024-01-16 01:20:33 -0300

    Programa Finalizado e Publicado

--- PraticaAPI/Program.cs
+++ PraticaAPI/Program.cs
@@ -1,75 +1,78 @@
-﻿using PraticaAPI.Filtro;
+﻿using PraticaAPI.Util;
 using PraticaAPI.Modelos;
 using System.Text.Json;
-
 
 Console.Title = $"Tempo Local";
 while (true)
 {
     Console.WriteLine("Digite uma cidade");
-    string? city = Console.ReadLine().ToUpper();
+    string? city = Console.ReadLine().ToUpperInvariant();
     using HttpClient chamada = new();
     string endPoint = $"https://api.openweathermap.org/data/2.5/weather?q={city}&lang=pt_br&appid={Functions.ApiKey()}&units=metric";
+
     try
     {
         string resposta = await chamada.GetStringAsync(endPoint);
         Root? cidade = JsonSerializer.Deserialize<Root>(resposta);
-        double tempCelcius = Functions.KelvinToCelcius(cidade!.Main.Temp);
-        double veloKM = Functions.MsToKm(cidade.Wind.Speed);
-        double tempSensacao = Functions.KelvinToCelcius(cidade!.Main.FeelsLike);
-        double tempMax = Functions.KelvinToCelcius(cidade!.Main.TempMax);
-        double tempMin = Functions.KelvinToCelcius(cidade!.Main.TempMin);
+        double tempCelcius = cidade!.Main.Temp;
+        double veloKM = cidade.Wind.Speed;
+        double tempSensacao = cidade!.Main.FeelsLike;
+        double tempMax = cidade!.Main.TempMax;
+        double tempMin = cidade!.Main.TempMin;
         int umidade = cidade!.Main.Humidity;
-        Weather listaTempo = cidade.Weather[0];
         string nome = cidade.Name.ToString();
-        string? CeuAtual = listaTempo.Main;
-        string? OlhaCeu = listaTempo.Description.ToUpperInvariant();
-        int? tempo = cidade.Timezone;
+        string? OlhaCeu = cidade.Weather[0].Description.ToUpperInvariant();
 
-
-        Console.Title = $"Tempo Local - {cidade.Name}";
-        Console.Clear();
-        Console.Beep(1020, 220);
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"\n\t{cidade.Name}");
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine($"Os ventos estão há {veloKM} Km/H");
-        Console.WriteLine($"Temperatura de {cidade!.Main.Temp}°C\nSensação de {cidade!.Main.FeelsLike}°C");
-        Console.WriteLine($"Umidade do ar: {umidade}%");
-        
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"\n\tPrevisão ");
-        Console.ForegroundColor = ConsoleColor.White;
-
-        Console.Write($"Temperatura Maxima: ");
-        Functions.MostraVermelho(cidade!.Main.TempMax.ToString());
-        Console.Write($"\nTemperatura Minima: ");
-        Functions.MostraAzul(cidade!.Main.TempMin.ToString());
-        int tt = Console.BufferHeight;
-        int yy = Console.BufferWidth;
-
-        Console.SetCursorPosition(19, 8);
-
-
-        Functions.MostraAmarelo(OlhaCeu);
-
-        ConsoleKeyInfo unused = Console.ReadKey();
-        Console.Clear();
+        MostraNaTela(cidade, veloKM, umidade, OlhaCeu);
     }
 
     catch (HttpRequestException)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Nome Nao Encontrado");
+        Console.WriteLine($"Local Não Encontrado");
         Console.Beep(400, 320);
         Console.ForegroundColor = ConsoleColor.White;
+        FecharApp();
+        Console.Clear();
+
     }
 }
-/*FAZER ESSA DEPOIS
 
-        string json = await harrypotter.GetStringAsync("https://hp-api.onrender.com/api/characters/house/gryffindor");
-        Console.WriteLine(json);
+static void MostraNaTela(Root? cidade, double veloKM, int umidade, string OlhaCeu)
+{
+    Console.Title = $"Tempo Local - {cidade.Name}";
+    Console.Clear();
+    Console.Beep(1020, 220);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"\n\t{cidade.Name}");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"Os ventos estão há {veloKM} Km/H");
+    Console.WriteLine($"Temperatura de {cidade!.Main.Temp}°C\nSensação de {cidade!.Main.FeelsLike}°C");
+    Console.WriteLine($"Umidade do ar: {umidade}%");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"\n\tPrevisão ");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.Write($"Temperatura Máxima: ");
+    Functions.MostraVermelho(cidade!.Main.TempMax.ToString());
+    Console.Write($"\nTemperatura Mínima: ");
+    Functions.MostraAzul(cidade!.Main.TempMin.ToString());
+
+    Console.SetCursorPosition(19, 7);
+    Functions.MostraAmarelo(OlhaCeu);
+    FecharApp();
+
+    Console.Clear();
 }
-*/
 
+static void FecharApp()
+{
+    Console.SetCursorPosition(3, 10);
+
+    Console.WriteLine($"\nPressione Insert para fechar");
+    ConsoleKeyInfo tecla = Console.ReadKey();
+
+    if (tecla.Key == ConsoleKey.Insert || tecla.KeyChar == '\0')
+        Environment.Exit(0);
+}
--- PraticaAPI/Util/Functions.cs
+++ PraticaAPI/Util/Functions.cs
@@ -1,32 +1,28 @@
-﻿namespace PraticaAPI.Filtro;
+﻿namespace PraticaAPI.Util;
 public class Functions
 {
     /// <summary>
     /// Chave disponibilizada para uso gratuito da API
     /// </summary>
-    public static string ApiKey() => "f24e3b3b88dc280e84e540c4500113d5";
-
+    public static string ApiKey() => "INSIRA SUA CHAVE AQUI";
     /// <summary>
     /// Realiza a conversao de temperatura Kelvin para Celcius.
     /// </summary>
     /// <param name="KelvinTemp">Temperatura em Escala Kelvin</param>
     /// <returns>Temperatura em Escala Celcius</returns>
     public static double KelvinToCelcius(double KelvinTemp) => Math.Round(KelvinTemp - 273.15, 2);
-
     /// <summary>
     /// Realiza a conversao de M/S para Km/H
     /// </summary>
     /// <param name="Speed">Velocidade em Metros por Segundo</param>
     /// <returns>Velocidade em Kilometro por Hora</returns>
     public static double MsToKm(double Speed) => Math.Round(Speed * 3.6, 2);
-
     public static void MostraVermelho(string palavra)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.Write($"{palavra}°C");
         Console.ForegroundColor = ConsoleColor.White;
     }
-
     public static void MostraAzul(string palavra)
     {
         Console.ForegroundColor = ConsoleColor.Blue;
